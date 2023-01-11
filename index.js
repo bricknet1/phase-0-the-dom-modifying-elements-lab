52c9fa3b187c4fac97b1499be6d3ba8437fcea0c
Nick Johnson <nlj222@gmail.com>
2023-01-11 10:27:39 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+//document.body.append(document.createElement('h1'));
+
+//const newHeader = document.querySelector('h1');
+
+//newHeader.id = 'victory';
+
+//newHeader.textContent = 'Nick is the champion';
+
+
+
+
+
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Nick is the champion"
+
+document.body.append(newHeader);
